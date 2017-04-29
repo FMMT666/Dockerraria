a7666cabad82687ff05eafa631036e15ee65ae57
FMMT666 <FMMT666@users.noreply.github.com>
2017-04-29 18:10:42 +0200

    improved overall usability

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM debian
 
-MAINTAINER FMMT666 <fmmt666@dev.null>
+MAINTAINER FMMT666 <fmmt666@users.noreply.github.com>
 
 RUN  apt-get -y update && \
      apt-get -y upgrade && \
@@ -9,11 +9,13 @@ RUN  apt-get -y update && \
      wget -O tserver.zip "http://terraria.org/server/terraria-server-1353.zip" && \
      unzip tserver.zip && \
      rm tserver.zip && \
-     chmod +x '/1353/Linux/TerrariaServer.bin.x86_64'
+     chmod +x '/1353/Linux/TerrariaServer.bin.x86_64' && \
+     rm -rf /1353/Mac && \
+     rm -rf /1353/Windows
 
 VOLUME /terraria
 
-COPY runthis.sh /
-COPY config.txt /
+COPY runthis.sh       /
+COPY serverconfig.txt /
 
 ENTRYPOINT ["/runthis.sh"]
--- runthis.sh
+++ runthis.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-  /usr/games/cowsay "This could be us, but playing with this Terraria 1.3.5.3 server. Just enter a Terraria world name to the command line. *burp*" 
+	/usr/games/cowsay "This could be us, but playing with this Terraria 1.3.5.3 server.\
+ Just enter the Terraria server parameters to the command line (type 'config' to use the\
+ 'terraria.cfg' file instead) or type 'bash' to get a shell. *burp*"
+elif [ "$1" == "bash" ]; then
+	/bin/bash -login
 else
-  # copy the default Terraris config file
-	if [ ! -f /terraria/config.txt ]; then
-     cp /config.txt /terraria
-  fi
+	# copy the default Terraria config file
+	if [ ! -f /terraria/terraria.cfg ]; then
+		cp /serverconfig.txt /terraria/terraria.cfg
+	fi
 
-  # add the magic part to modify the Terraria config.txt
-  # ...
+	if [ "$1" == "config" ]; then
+		/1353/Linux/TerrariaServer.bin.x86_64 -config /terraria/terraria.cfg
+	else
+		/1353/Linux/TerrariaServer.bin.x86_64 "$@"
+	fi
 
-  # start the server
-	/1353/Linux/TerrariaServer.bin.x86_64 -config /terraria/config.txt
+	# debug only
+#	/bin/bash -login
 
-  # fall back to this shell (yet)
-#  /bin/bash -login
-    
 fi
